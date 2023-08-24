567a700efc145b456ea9b6e0a282bfcf74f8213b
tuzhuyue <wxs290tzy@gmail.com>
2023-08-24 15:31:30 +0800

    常量

---
+++ src/main/java/com/example/usercenter1/constant/UserConstant.java
@@ -0,0 +1,18 @@
+package com.example.usercenter1.constant;
+
+public interface UserConstant {
+    /**
+     * 用户登录状态键
+     */
+    static final String USER_LOGIN_STATE = "userLoginState";
+
+    //----------权限----------
+    /**
+     * 默认权限
+     */
+    int DEFAULT_ROLE = 0;
+    /**
+     * 管理员权限
+     */
+    int ADMIN_ROLE = 1;
+}
